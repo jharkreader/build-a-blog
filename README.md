b1ce3d01d8935c6469c4a8da3bb3d2396a93a7fc
asclepiusgal <jeanna.e.clark@gmail.com>
2017-06-28 18:45:40 -0700

    code-review

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-# build-a-blog
+# Blog App
+## A simple Flask project for LC-101 students
 
 This is an assignment for LaunchCode LC-101 demonstrating a dynamic web app that adds and displays blog posts.  Utilizes python/Flask/MAMP.
+
+# _Implements_:
+
+- [Flask](http://flask.pocoo.org/) template rendering
+- manual form validation
+- HTTP routing & endpoints
+- HTML
+- CSS
+- request
+- redirect
+- render_template (in place of jinja2)
+- mySQL database
+- SQLAlchemy 
+- cookie sessions
+
+***
+
+# SQLAlchemy setup & usage
+
+open python3 in terminal
+>>> from main import db,Task 
+>>> db
+>>> db.create_all()  ### creates the DB using the specified columns
+>>> new_task = Task('finish Lesson 2')
+>>> db.session.add(new_task) 
+>>> new_task_2 = Task('finish Lesson 3')
+>>> db.session.add(new_task_2) 
+>>> db.session.commit() ### adds the new_task variable to database
+>>> tasks = task.query.all() ### selects everything from the database
+>>> tasks[0].name = 'finish Lesson 2'
+>>> db.session.delete(new_task_2)
+>>> db.drop_all() ### drops all the tables
+>>> db.create_all() ### creates the database with the new / added columns
+
+open mysql in another terminal window 
+> create database blog;
+> use blog; ## uses the blog database
+> show tables; ## shows the tables
+> describe task; ## shows the table columns
+> select * from task; ## shows the new data inserted into the table columns
+
+***
+
+## _Sources_
+
+- _Project rubric_: [LC-101 Blog App](http://education.launchcode.org/web-fundamentals/assignments/build-a-blog/)
+
+***
+
+(c) June 2017
--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+#!/usr/bin/env python
+
+__author__ = "student"
+__version__ = "1.0"
+# June 26, 2017
+# Flask Blog App re: LaunchCode LC-101
+# Rubric: http://education.launchcode.org/web-fundamentals/assignments/build-a-blog/
+
+
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:insecure@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'qwer654Beta##'
+app.secret_key = 'super_secret_key'
 
 
 class BlogPost(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     blog_title = db.Column(db.String(120))
     blog_text = db.Column(db.String(1024))
+    date = db.Column(db.DateTime)
 
-    def __init__(self, blog_title, blog_text):
+    def __init__(self, blog_title, blog_text, date=None):
         self.blog_title = blog_title
         self.blog_text = blog_text
+        if date is None:
+            date = datetime.utcnow()
+        self.date = date
 
 
 @app.route('/blog', methods=['GET', 'POST'])
@@ -42,11 +54,11 @@ def add_blog():
         title_error = ''
         text_error = ''
 
-        if blog_title == "":
+        if blog_title == '':
             title_error = "Please enter a title."
-        if blog_text == "":
-            text_error = "Please enter blog text."    
-        
+        if blog_text == '':
+            text_error = "Please enter blog text."
+
         if not title_error and not text_error:
 
             new_blog = BlogPost(blog_title, blog_text)
@@ -57,15 +69,11 @@ def add_blog():
             return redirect('/blog?id={0}'.format(id_param))
 
         else:
-            return render_template('newpost.html', 
-            title_error=title_error,
-            text_error=text_error, 
-            blog_text=blog_text, 
-            blog_title=blog_title)    
-
+            return render_template('newpost.html', title_error=title_error, text_error=text_error,
+                                   blog_text=blog_text, blog_title=blog_title)
     else:
         return render_template('newpost.html')
 
 
 if __name__ == '__main__':
-    app.run()  
+    app.run()
